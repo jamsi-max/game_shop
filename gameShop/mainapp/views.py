ce5da633f79c345d1dbf9f3a15129b3295a7b8c4
jamsi-max <jamsi@bk.ru>
2020-04-06 15:46:21 +0300

    add json data

--- gameShop/mainapp/views.py
+++ gameShop/mainapp/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 
+# function upload data from file json
+import json
+import os
+def get_data():
+    try:
+        data = json.load(open(os.path.abspath(r'gameShop\data.json'), encoding="utf-8"))
+    except:
+        print('Error load data from BD')
+        data = []
+    return data
+
+LINKS_MENU = [
+    {'name': 'home', 'href':'index'},
+    {'name': 'gallery','href':'products'},
+    {'name': 'Contact Us', 'href':'contact'}
+]
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    data = get_data()
+    print(data)
+    content = {
+        'page_title': 'главная',
+        'links_menu': LINKS_MENU
+    }
+    return render(request, 'mainapp/index.html', context=content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'page_title': 'каталог',
+        'links_menu': LINKS_MENU
+    }
+    return render(request, 'mainapp/products.html', context=content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    content = {
+        'page_title': 'контакты',
+        'links_menu': LINKS_MENU
+    }
+    return render(request, 'mainapp/contact.html', context=content)
